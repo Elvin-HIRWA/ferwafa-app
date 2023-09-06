aac58a23cbd6efba675d681066f69b2ba1290a9b
Elvin-HIRWA <elhirwa3@gmail.com.com>
2023-09-06 09:35:09 +0200

    improved what u see is what u get

--- resources/views/admin/create-news.blade.php
+++ resources/views/admin/create-news.blade.php
@@ -101,7 +101,7 @@
                                                 <div class="form-group row mb-4">
                                                     <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
                                                     <div class="col-sm-12 col-md-7">
-                                                        <textarea name="description" class="summernote-simple"></textarea>
+                                                        <textarea name="description" class="summernote"></textarea>
                                                         @error('description')
                                                         <div style="color: red;">
                                                             {{ $message }}
