d3a3f0a568e492bd20dd0567911e4df83a66baf0
Elvin-HIRWA <elhirwa3@gmail.com.com>
2023-10-18 09:43:17 +0200

    women show day for recent game

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -17,14 +17,27 @@
 
 <body>
     @php
-        $day = DB::table('Game')
-            ->join('Day', 'Day.id', '=', 'Game.dayID')
-            ->where('Game.isPlayed', 1)
-            ->orderBy('Day.id', 'DESC')
-            ->first(['Game.dayID']);
-        $competions = DB::table('TeamCategory')
-            ->select('id', 'name')
-            ->get();
+    $competions = DB::table('TeamCategory')
+    ->select('id', 'name')
+    ->get();
+    $menDay = DB::table('Game')
+    ->join('Day', 'Day.id', '=', 'Game.dayID')
+    ->join('Team','Team.id', '=', 'homeTeamID')
+    ->join('TeamCategory', 'Team.categoryID', '=','TeamCategory.id')
+    ->where('Game.isPlayed', 1)
+    ->where('TeamCategory.id',$competions[0]->id)
+    ->orderBy('Day.id', 'DESC')
+    ->first(['Game.dayID']);
+
+    $womenDay = DB::table('Game')
+    ->join('Day', 'Day.id', '=', 'Game.dayID')
+    ->join('Team','Team.id', '=', 'homeTeamID')
+    ->join('TeamCategory', 'Team.categoryID', '=','TeamCategory.id')
+    ->where('Game.isPlayed', 1)
+    ->where('TeamCategory.id',$competions[1]->id)
+    ->orderBy('Day.id', 'DESC')
+    ->first(['Game.dayID']);
+
     @endphp
     <header>
         <div class="headerbox">
@@ -32,29 +45,22 @@
                 <div class="row justify-content-between align-items-center;">
                     <div class="col">
                         <div style="" class="logo">
-                            <a href="/" title="Return Home"><img
-                                    style="height: 90px; margin-bottom: 100px; margin-left: 20px" alt="Logo"
-                                    class="logo_img" src="{{ asset('static/img/federation/ferwafa.png') }}" /></a>
+                            <a href="/" title="Return Home"><img style="height: 90px; margin-bottom: 100px; margin-left: 20px" alt="Logo" class="logo_img" src="{{ asset('static/img/federation/ferwafa.png') }}" /></a>
                         </div>
                     </div>
                     <div class="col" style="display: flex;">
                         <div style="margin-top: 20px;">
                             <marquee behavior="" direction="left">
-                                @if ($day)
-                                    <h2 style="color:  #133E8D"> <a
-                                            href="{{ route('fixtures.show', [$competions[0]->id, $day->dayID]) }}"
-                                            style="color:  #133E8D">About Primus National League Click here</a></h2>
+                                @if ($menDay)
+                                <h2 style="color:  #133E8D"> <a href="{{ route('fixtures.show', [$competions[0]->id, $menDay->dayID]) }}" style="color:  #133E8D">About Primus National League Click here</a></h2>
                                 @else
-                                    <h2 style="color:  #133E8D"> <a href="{{ route('fixtures.show', [$competions[0]->id, $day->dayID]) }}"
-                                            style="color:  #133E8D">About Primus National League Click here</a></h2>
+                                <h2 style="color:  #133E8D"> <a href="{{ route('fixtures.show', [$competions[0]->id, $menDay->dayID]) }}" style="color:  #133E8D">About Primus National League Click here</a></h2>
                                 @endif
                             </marquee>
                         </div>
                     </div>
                     <div class="col">
-                        <img alt="" height="100px" width="" class="img-responsive banner-image"
-                            src="{{ asset('static/img/federation/primus.png') }}" /><a class="mobile-nav"
-                            href="#mobile-nav"><i class="fa fa-bars"></i></a>
+                        <img alt="" height="100px" width="" class="img-responsive banner-image" src="{{ asset('static/img/federation/primus.png') }}" /><a class="mobile-nav" href="#mobile-nav"><i class="fa fa-bars"></i></a>
                     </div>
                 </div>
             </div>
@@ -70,13 +76,13 @@
                     <a href="">Competitions</a>
                     <ul class="sub-current">
                         <li>
-                            <a href="#">{{$competions[0]->name}}</a>
+                            <a href="#">{{ $competions[0]->name}}</a>
                             <ul class="sub-current">
                                 @if ($day)
-                                    <li><a href="{{ route('fixtures.show', [$competions[0]->id, $day->dayID]) }}">Primus
-                                            National</a></li>
+                                <li><a href="{{ route('fixtures.show', [$competions[0]->id, $menDay->dayID]) }}">Primus
+                                        National</a></li>
                                 @else
-                                    <li><a href="{{ route('fixtures.show', [$competions[0]->id, 1]) }}">Primus National</a></li>
+                                <li><a href="{{ route('fixtures.show', [$competions[0]->id, 1]) }}">Primus National</a></li>
                                 @endif
                                 <li><a href="#">Second Division</a></li>
                                 <li><a href="#">Third Division</a></li>
@@ -86,9 +92,9 @@
                             <a href="#">{{$competions[1]->name}}</a>
                             <ul class="sub-current">
                                 @if ($day)
-                                    <li><a href="{{ route('fixtures.show', [$competions[1]->id, $day->dayID]) }}">First Division</a></li>
+                                <li><a href="{{ route('fixtures.show', [$competions[1]->id, $womenDay->dayID]) }}">First Division</a></li>
                                 @else
-                                    <li><a href="{{ route('fixtures.show', [$competions[1]->id, 1]) }}">First Division</a></li>
+                                <li><a href="{{ route('fixtures.show', [$competions[1]->id, 1]) }}">First Division</a></li>
                                 @endif
                                 <li><a href="#">Second Division</a></li>
                             </ul>
@@ -170,9 +176,9 @@
                     </ul>
                 </li>
                 @if (!Auth::check())
-                    <li class="">
-                        <a href="{{ route('login') }}">Login</a>
-                    </li>
+                <li class="">
+                    <a href="{{ route('login') }}">Login</a>
+                </li>
                 @endif
             </ul>
             <!-- End Menu-->
@@ -190,10 +196,10 @@
                         <a href="#">{{$competions[0]->name}}</a>
                         <ul class="#">
                             @if ($day)
-                                <li><a href="{{ route('fixtures.show', [$competions[0]->id, $day->dayID]) }}">Primus
-                                        National</a></li>
+                            <li><a href="{{ route('fixtures.show', [$competions[0]->id, $menDay->dayID]) }}">Primus
+                                    National</a></li>
                             @else
-                                <li><a href="{{ route('fixtures.show', [$competions[0]->id, 1]) }}">Primus National</a></li>
+                            <li><a href="{{ route('fixtures.show', [$competions[0]->id, 1]) }}">Primus National</a></li>
                             @endif
                             <li><a href="#">Second Division</a></li>
                             <li><a href="#">Third Division</a></li>
@@ -203,9 +209,9 @@
                         <a href="#">{{$competions[1]->name}}</a>
                         <ul class="#">
                             @if ($day)
-                                <li><a href="{{ route('fixtures.show', [$competions[1]->id, $day->dayID]) }}">First Division</a></li>
+                            <li><a href="{{ route('fixtures.show', [$competions[1]->id, $womenDay->dayID]) }}">First Division</a></li>
                             @else
-                                <li><a href="{{ route('fixtures.show', [$competions[1]->id, 1]) }}">First Division</a></li>
+                            <li><a href="{{ route('fixtures.show', [$competions[1]->id, 1]) }}">First Division</a></li>
                             @endif
                             <li><a href="#">Second Division</a></li>
                         </ul>
@@ -287,9 +293,9 @@
                 </ul>
             </li>
             @if (!Auth::check())
-                <li class="">
-                    <a href="{{ route('login') }}">Login</a>
-                </li>
+            <li class="">
+                <a href="{{ route('login') }}">Login</a>
+            </li>
             @endif
         </ul>
         <!-- End Menu-->
